adeab97d5f47cf1c1b364c354dd919ed4309f3b0
sefaonder <ondersefa111@gmail.com>
2024-04-23 17:57:16 +0300

    profile render bug solved

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,33 +1,18 @@
 import { useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { useGetProfileMutation } from 'src/store/services/profile/ProfileApiSlice';
+import { useGetProfileQuery } from 'src/store/services/profile/ProfileApiSlice';
 import { setProfile } from 'src/store/services/profile/ProfileSlice';
 
 function Dashboard() {
-  const [getProfile, { isLoading }] = useGetProfileMutation();
+  const { data, isLoading, isSuccess, isError, error } = useGetProfileQuery();
 
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const getUser = async () => {
-      try {
-        const response = await getProfile();
-        if (response.error) {
-          throw new Error(response.error.message);
-        }
-
-        const userData = response.data; // Assuming data is stored in response.data
-        if (userData) {
-          dispatch(setProfile(userData));
-        } else {
-          console.log('User data is null or undefined.');
-        }
-      } catch (err) {
-        console.log('Error occurred while fetching user data:', err);
-      }
-    };
-    getUser();
-  }, [getProfile, dispatch]);
+    if (isSuccess && data.data) {
+      dispatch(setProfile(data));
+    }
+  }, [isSuccess]);
 
   return <div>asd asdasd</div>;
 }
