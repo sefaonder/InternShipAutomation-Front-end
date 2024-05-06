d4f74cbfd32575ef02636e0a8f4e194fbef7824c
Murat İlhan <murattilhann08@gmail.com>
2024-05-06 12:35:40 +0300

    dasboard updated

--- src/components/diagram/Diagram.jsx
+++ src/components/diagram/Diagram.jsx
@@ -13,30 +13,11 @@ const Diagram = () => {
   ];
   const status = 4;
   return (
-    <div>
-      <Typography variant="h2">Staj Formu</Typography>
-      <Paper
-        sx={{
-          display: 'flex',
-          flexDirection: 'row-reverse',
-          padding: '1rem',
-          marginBottom: '1rem',
-          marginTop: '1rem',
-          overflowX: 'auto',
-          gap: '1rem',
-        }}
-      ></Paper>
-      <Box className="flex flex-col sm:flex-row gap-4">
-        <Paper sx={{ flex: 2, padding: '1rem' }}>
-          <Box className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
-            {diagramData.map((item, index) => (
-              <DiagramBox index={index + 1} text={item.text} success={status > index ? true : false} />
-            ))}
-          </Box>
-        </Paper>
-        <Box className="flex flex-1 flex-col">{/* <RecordTraceCard record={internFormData} /> */}</Box>
-      </Box>
-    </div>
+    <Box className="grid grid-cols-[repeat(auto-fit,minmax(200px,1fr))]">
+      {diagramData.map((item, index) => (
+        <DiagramBox index={index + 1} text={item.text} success={status > index ? true : false} />
+      ))}
+    </Box>
   );
 };
 
--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,8 +1,8 @@
+import { Box, Paper, Typography } from '@mui/material';
 import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import PdfConfidentalReport from 'src/PDF/PdfConfidentalReport';
-import PdfSurvey from 'src/PDF/PdfSurvey';
+import Diagram from 'src/components/diagram/Diagram';
 import { useGetProfileQuery } from 'src/store/services/profile/ProfileApiSlice';
 import { setProfile } from 'src/store/services/profile/ProfileSlice';
 function Dashboard() {
@@ -20,12 +20,24 @@ function Dashboard() {
 
   return (
     <div>
-      {/* <PDFDownloadLink fileName="FORM" document={<PdfSurvey data={res} />}>
-        {({ loading }) => (loading ? <button>loading Document..</button> : <button> download </button>)}
-      </PDFDownloadLink> */}
-      <PDFViewer style={{ width: '100%', height: '1000px  ' }}>
-        <PdfSurvey data={res} />
-      </PDFViewer>
+      <Typography variant="h2">Staj Formu</Typography>
+      <Paper
+        sx={{
+          display: 'flex',
+          flexDirection: 'row-reverse',
+          padding: '1rem',
+          marginBottom: '1rem',
+          marginTop: '1rem',
+          overflowX: 'auto',
+          gap: '1rem',
+        }}
+      ></Paper>
+      <Box className="flex flex-col sm:flex-row gap-4">
+        <Paper sx={{ flex: 2, padding: '1rem' }}>
+          <Diagram></Diagram>
+        </Paper>
+        <Box className="flex flex-1 flex-col"></Box>
+      </Box>
     </div>
   );
 }
