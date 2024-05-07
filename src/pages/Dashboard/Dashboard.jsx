f8f4e9c26f82107a1266bed8f386298f4ae0735f
Murat İlhan <murattilhann08@gmail.com>
2024-05-07 15:15:37 +0300

    last commit

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,5 +1,8 @@
+import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import PdfConfidentalReport from 'src/PDF/PdfConfidentalReport';
+import PdfSurvey from 'src/PDF/PdfSurvey';
 import { useGetProfileQuery } from 'src/store/services/profile/ProfileApiSlice';
 import { setProfile } from 'src/store/services/profile/ProfileSlice';
 function Dashboard() {
@@ -15,7 +18,16 @@ function Dashboard() {
     }
   }, [isSuccess]);
 
-  return <div></div>;
+  return (
+    <div>
+      {/* <PDFDownloadLink fileName="FORM" document={<PdfSurvey data={res} />}>
+        {({ loading }) => (loading ? <button>loading Document..</button> : <button> download </button>)}
+      </PDFDownloadLink> */}
+      <PDFViewer style={{ width: '100%', height: '1000px  ' }}>
+        <PdfSurvey data={res} />
+      </PDFViewer>
+    </div>
+  );
 }
 
 export default Dashboard;
