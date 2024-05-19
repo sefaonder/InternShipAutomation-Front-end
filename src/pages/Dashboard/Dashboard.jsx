90a626227b9299a1882d66a6fc0da6157a5f1310
sefaonder <ondersefa111@gmail.com>
2024-05-19 07:36:28 +0300

    dashboard page updated

--- src/pages/Dashboard/Dashboard.jsx
+++ src/pages/Dashboard/Dashboard.jsx
@@ -1,41 +1,39 @@
 import { Box, Paper, Typography } from '@mui/material';
-import { PDFDownloadLink, PDFViewer } from '@react-pdf/renderer';
-import { useEffect } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import Diagram from 'src/components/diagram/Diagram';
-import { useGetProfileQuery } from 'src/store/services/profile/ProfileApiSlice';
-import { setProfile } from 'src/store/services/profile/ProfileSlice';
-function Dashboard() {
-  const { data, isLoading, isSuccess, isError, error } = useGetProfileQuery();
-  const internform = useSelector((state) => state.internForm);
-  console.log(internform);
 
+import { useDispatch } from 'react-redux';
+import { UserRolesEnum } from 'src/app/enums/roleList';
+import Diagram from 'src/components/diagram/Diagram';
+import usePermission from 'src/hooks/usePermission';
+
+function Dashboard() {
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    if (isSuccess && data.data) {
-      dispatch(setProfile(data));
-    }
-  }, [isSuccess]);
+  const checkPermission = usePermission();
+
+  const isAdvancedComission = checkPermission(UserRolesEnum.COMISSION.id);
+  const isAdvancedAdmin = checkPermission(UserRolesEnum.ADMIN.id);
 
   return (
     <div>
-      <Typography variant="h2">Staj Formu</Typography>
+      <Typography variant="h2">Bursa Uludağ Üniversitesi Bilgisayar Mühendisliği Staj Otomasyonu</Typography>
       <Paper
         sx={{
           display: 'flex',
-          flexDirection: 'row-reverse',
           padding: '1rem',
           marginBottom: '1rem',
           marginTop: '1rem',
           overflowX: 'auto',
           gap: '1rem',
         }}
-      ></Paper>
+      >
+        Bilgilendirme yazısı
+      </Paper>
       <Box className="flex flex-col sm:flex-row gap-4">
-        <Paper sx={{ flex: 2, padding: '1rem' }}>
-          <Diagram></Diagram>
-        </Paper>
+        {!isAdvancedComission && (
+          <Paper sx={{ flex: 2, padding: '1rem' }}>
+            <Diagram />
+          </Paper>
+        )}
         <Box className="flex flex-1 flex-col"></Box>
       </Box>
     </div>
