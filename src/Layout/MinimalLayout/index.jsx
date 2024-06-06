5bca9ee070e48f9c3a87ca443218e92217d3acd1
sefaonder <ondersefa111@gmail.com>
2024-06-06 14:43:51 +0300

    image bug solved

--- src/Layout/MinimalLayout/index.jsx
+++ src/Layout/MinimalLayout/index.jsx
@@ -9,7 +9,7 @@ const MinimalLayout = () => {
       {!checkCompany ? (
         <div
           className="absolute flex items-center justify-center inset-0 bg-cover bg-no-repeat bg-center opacity-80"
-          style={{ backgroundImage: "url('/public/images/uludagPhoto.jpg')" }}
+          style={{ backgroundImage: "url('/images/uludagPhoto.jpg')" }}
         >
           <Outlet />
         </div>
